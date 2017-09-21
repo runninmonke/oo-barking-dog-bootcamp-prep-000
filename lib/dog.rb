45ec0647ea836d13ef9313fd2589be8b2c45fd5b
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-21 21:49:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+
+class Dog
+
+  def initialize(intial_name, initial_breed)
+    @name = intial_name
+    @breed = initial_breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def bark()
+    puts "Woof!"
+  end
+
+end
