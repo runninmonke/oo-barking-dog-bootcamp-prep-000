a8fffc8b784b8ff39ef4feb6c76781f76fbc8e64
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-21 21:52:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,16 +10,8 @@ class Dog
     @name = new_name
   end
 
-  def breed
-    @breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-
   def bark()
-    puts "Woof!"
+    puts "woof!"
   end
 
 end
